9dc66618345e042a8453a5f186ce121fadfeed95
limap <paulo.xavier@parnamirim.rn.gov.br>
2018-04-25 11:54:06 -0300

    Atualizei o arquivo

--- const.js
+++ const.js
@@ -1,10 +1,1 @@
 const PI = 3.141593;
-PI > 3.0
-
-Object.defineProperty(typeof global === "object" ? global : window, "PI", {
-	value: 3.141593,
-	enumerable: true,
-	writable: false,
-	configurable: false
-})
-PI > 3.0;
